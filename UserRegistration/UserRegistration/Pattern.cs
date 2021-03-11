081d6088ab8764cd90bf09927b5e50b4d467ed10
unknown <nandiniswaraj13@gmail.com>
2021-03-11 21:39:29 +0530

    [Nandini swaraj]Added  Email Validation

--- UserRegistration/UserRegistration/Pattern.cs
+++ UserRegistration/UserRegistration/Pattern.cs
@@ -28,6 +28,18 @@ namespace UserRegistration
                 return false;
 
         }
+        public bool isValidEmail(string email)
+        {
+            string Regex_email = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+
+            Regex regex = new Regex(Regex_email);
+            if (regex.IsMatch(email))
+                return true;
+            else
+                return false;
+
+        }
+
 
     }
 }
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,11 +7,13 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Pattern pattern = new Pattern();
-            Console.WriteLine(pattern.isValidLastName("Abcdef"));
-            Console.WriteLine(pattern.isValidLastName("abc"));
-            Console.WriteLine(pattern.isValidLastName("ABC"));
-            Console.WriteLine(pattern.isValidLastName("AB"));
-
+            Console.WriteLine(pattern.isValidEmail("abc.xyz@bl.co.in"));
+            Console.WriteLine(pattern.isValidEmail("abc@bl.co.in"));
+            Console.WriteLine(pattern.isValidEmail("abc.xyz@bl.co"));
+            Console.WriteLine(pattern.isValidEmail("abc.xyz@bl.co"));
+            Console.WriteLine(pattern.isValidEmail("abc@bl.co.in.ap"));
+            Console.WriteLine(pattern.isValidEmail("abc@gmail.co"));
+            Console.WriteLine(pattern.isValidEmail("nandiniswaraj1995@gmail.com"));
 
 
         }
